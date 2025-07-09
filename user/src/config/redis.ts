40a73a7a78afb8be28869673422ceb038e4f9fd0
Lavish <lavishgarg1199@gmail.com>
2025-07-09 16:55:53 +0530

    refactor redis configuration file

--- user/src/config/redis.ts
+++ user/src/config/redis.ts
@@ -3,6 +3,22 @@ dotenv.config()
 
 import {createClient} from 'redis'
 
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error('REDIS_URL environment variable is required');
+}
 export const redisClient = createClient({
-  url: process.env.REDIS_URL
-})
+  url: redisUrl,
+});
+
+
+const conntectRedis = async() => {
+ try {
+   await redisClient.connect()
+   console.log('Redis connected')
+ } catch (error) {
+   console.error('Failed to connect to redis', error)
+ }
+}
+
+export default conntectRedis
--- user/src/index.ts
+++ user/src/index.ts
@@ -1,16 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
-import { redisClient } from "./config/redis.js";
+import conntectRedis from "./config/redis.js";
 
 dotenv.config()
+
 await connectDB()
-try {
-  await redisClient.connect()
-  console.log('Redis connected')
-} catch (error) {
-  console.error('Failed to connect to redis', error)
-}
+await conntectRedis()
+
 const app = express();
 
 const port = process.env.PORT || 5000;
